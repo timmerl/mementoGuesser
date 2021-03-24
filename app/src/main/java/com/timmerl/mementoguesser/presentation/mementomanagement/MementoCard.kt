7752f1e40b3e05439492d5888c2b30fc72e55dbc
Plab <timmerl.dev@gmail.com>
2021-03-24 17:08:17 +0100

    remove useless

--- app/src/main/java/com/timmerl/mementoguesser/presentation/mementomanagement/MementoCard.kt
+++ app/src/main/java/com/timmerl/mementoguesser/presentation/mementomanagement/MementoCard.kt
@@ -1,4 +1,4 @@
-package com.timmerl.mementoguesser.presentation.composable
+package com.timmerl.mementoguesser.presentation.mementomanagement
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
