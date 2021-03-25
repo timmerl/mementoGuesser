ab88462adcd23b4d4e9677fa0684a1139e9e3c09
Plab <timmerl.dev@gmail.com>
2021-03-25 13:55:43 +0100

    v1

---
+++ app/src/main/java/com/timmerl/mementoguesser/presentation/mementoguesser/MementoGuesserScreen.kt
@@ -0,0 +1,10 @@
+package com.timmerl.mementoguesser.presentation.mementoguesser
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MementoGuesserScreen(
+    viewModel: MementoGuesserViewModel
+) {
+
+}
