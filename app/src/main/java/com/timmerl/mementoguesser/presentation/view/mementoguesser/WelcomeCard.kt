f05926cf2e333a572dff3981877983abb0589cf8
Plab <timmerl.dev@gmail.com>
2021-03-31 12:53:58 +0200

    welcomeCard remove ripple effect

--- app/src/main/java/com/timmerl/mementoguesser/presentation/view/mementoguesser/WelcomeCard.kt
+++ app/src/main/java/com/timmerl/mementoguesser/presentation/view/mementoguesser/WelcomeCard.kt
@@ -1,6 +1,7 @@
 package com.timmerl.mementoguesser.presentation.view.mementoguesser
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -25,7 +27,11 @@ fun WelcomeCard(
             .fillMaxHeight()
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable(onClick = onClicked)
+            .clickable(
+                onClick = onClicked,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            )
     ) {
         Text(
             text = LocalContext.current.getString(R.string.welcome_message),
